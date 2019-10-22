1bcd7178eec3cb8c825c00e1ed2457842fdec980
aammokt <55858810+aammokt@users.noreply.github.com>
2019-10-22 20:04:09 +0800

    Update 1_large.py

--- python102/large/1_large.py
+++ python102/large/1_large.py
@@ -1,1 +1,23 @@
-print
+SIZE = 3
+board = []
+
+for y in range(SIZE):
+        board.append([])
+        for x in range(SIZE):
+                board[y].append("[%d][%d]" % (y,x))
+
+
+for row in board:
+        for column in row:
+                print("%s " % column, end="")
+        print("\n")
+
+print("\n\nPlayer X is moving. \n\n")
+board[0][2] = "X"
+user_move_x = int(input("Please enter your move (x-coordinate): "))
+user_move_y = int(input("Please enter your move (y-coordinate): "))
+board[user_move_x][user_move_y] = "X"
+for row in board:
+        for column in row:
+                print("%s " % column, end="")
+        print("\n")
