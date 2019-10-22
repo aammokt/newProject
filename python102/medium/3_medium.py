ff6b1334291c0903b6bba5d30f7add4c72351090
aammokt <55858810+aammokt@users.noreply.github.com>
2019-10-22 11:04:21 +0800

    Update 3_medium.py

--- python102/medium/3_medium.py
+++ python102/medium/3_medium.py
@@ -1,21 +1,38 @@
-list1 = input("Please enter 1st matrix: ")
-print(type(list1))
-# list2 = [[5,2], [1,0]]
-# new_list1 = []
-# new_list2 = []
-# result_list = []
-# x = len(list1)
-# y = len(list2)
+SIZE = int(input("Please enter the size of the matrix to be added: "))
+board1 = []
+board2 = []
+result_board = []
 
-# for i in range(x):
-#     for j in range(y):
-#         if i == 0:
-#             num = list1[i][j] + list2[i][j]
-#             new_list1.append(num)
-#         else:
-#             num = list1[i][j] + list2[i][j]
-#             new_list2.append(num)
-# result_list.append(new_list1)
-# result_list.append(new_list2)
+for y in range(SIZE):
+        board1.append([])
+        for x in range(SIZE):
+                user_num = input(f'Please enter input for [{y}][{x}] on matrix 1: ')
+                board1[y].append(user_num)
 
-# print(f'Result of matrix addition of {list1} and {list2} is {result_list}')
+for row in board1:
+        for column in row:
+                print(f'{column}  ', end="")
+        print("\n")
+
+for y in range(SIZE):
+        board2.append([])
+        for x in range(SIZE):
+                user_num = input(f'Please enter input for [{y}][{x}] on matrix 2: ')
+                board2[y].append(user_num)
+
+for row in board2:
+        for column in row:
+                print(f'{column}  ', end="")
+        print("\n")
+
+for y in range(SIZE):
+        result_board.append([])
+        for x in range(SIZE):
+                new_sum = int(board1[x][y]) + int(board2[x][y])
+                result_board[y].append(new_sum)
+
+print("The result is sum between the two matrix are: \n")
+for row in result_board:
+        for column in row:
+                print(f'{column}  ', end="")
+        print("\n")
